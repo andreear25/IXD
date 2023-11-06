d401ba1a512ae679e9da68cd27c537eb156bd010
andreear25 <118562969+andreear25@users.noreply.github.com>
2023-11-07 01:16:19 +0200

    Car program

---
+++ XD/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxspeed;
+        public int year;
+
+        public Car(string nameName, string colorName, int yearValue, int maxspeedValue)
+        {
+           color = colorName;
+           name = nameName;
+           maxspeed = maxspeedValue;
+           year = yearValue;
+        }
+        public Car(string nameName)
+        {
+            name= nameName;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + "is going at yhe speed of" + maxspeed);
+        }
+    }
+}
